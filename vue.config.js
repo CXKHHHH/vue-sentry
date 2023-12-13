07d4593aa077a505e35b80fdaff0fa682b694a96
CXKHHHH <2096520886@qq.com>
2023-12-13 16:53:35 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
+const  { sentryWebpackPlugin }  =  require ( "@sentry/webpack-plugin" ) ; 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack:{
+    devtool : "source-map" ,  // 必须打开源映射生成
+    plugins : [ 
+      sentryWebpackPlugin ( { 
+        org:"qweda",
+        project :"javascript-vue",
+        authToken:"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"
+      } ) , 
+    ] , 
+  }
 });
